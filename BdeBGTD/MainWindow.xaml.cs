b84d43a5e806ae0278b37a67a3a3c8af8c3a4613
Malak Fora <2067247@bdeb.qc.ca>
2023-11-01 10:54:56 -0400

    J'ai creer l'interface et creer les methodes par rapport a l'affichage et au changement de la date

--- BdeBGTD/MainWindow.xaml.cs
+++ BdeBGTD/MainWindow.xaml.cs
@@ -20,9 +20,28 @@ namespace BdeBGTD
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DateTime dateAffichee;
         public MainWindow()
         {
+        InitializeComponent();
+
+            // date.Text = DateTime.Now.ToShortDateString();
+            date.Text = dateAffichee.ToShortDateString();
             InitializeComponent();
+           dateAffichee = DateTime.Now;
+            ChangerDate(dateAffichee);
+        }
+
+        private void PlusButton_Click(object sender, RoutedEventArgs e)
+        {
+        //Ajoute 2 jours a la date affichée
+            dateAffichee = dateAffichee.AddDays(2);
+            ChangerDate(dateAffichee);
+        }
+        //met à jour la date àpres l'ajout de 2 jours
+        private void ChangerDate(DateTime nouvelleDate)
+        {
+            date.Text = nouvelleDate.ToShortDateString();
         }
     }
 }
